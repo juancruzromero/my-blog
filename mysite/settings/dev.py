1f6287cd7dbc578007aca98f1f2720fb7fe075ef
Juan Cruz <juancruz.romero.dev@gmail.com>
2026-02-08 21:00:07 -0300

    Add new files for refactor

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!#2*6mvd@l_7fs8^fg_^@ob8***hw4o^$=oiwd^h1nja6c8d*u"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
